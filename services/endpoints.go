97898bf77bb1caacdab43d5690e20d37a5f9f9f1
Tony Yip <tony@opensource.hk>
2017-11-12 23:11:26 +0800

    Add get service status

---
+++ services/endpoints.go
@@ -0,0 +1,43 @@
+package services
+
+import (
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/pkg/api/v1"
+)
+
+func createPodInfo(addresses []v1.EndpointAddress) *podInfo {
+	names := make([]string, 0)
+	for _, pod := range addresses {
+		names = append(names, pod.TargetRef.Name)
+	}
+
+	return &podInfo{
+		Total: len(addresses),
+		Pods:  names,
+	}
+}
+
+
+func GetServiceStatus(client corev1.CoreV1Client, namespace, name string) (*componentEndpoints, error) {
+	endpoint, err := client.Endpoints(namespace).Get(name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	subset := endpoint.Subsets[0]
+
+	notReady := subset.NotReadyAddresses
+	notReadyPods := createPodInfo(notReady)
+
+	ready := subset.Addresses
+	readyPods := createPodInfo(ready)
+
+	resp := componentEndpoints{
+		Total:        len(ready) + len(notReady),
+		Available:    readyPods,
+		NotAvailable: notReadyPods,
+	}
+
+	return &resp, nil
+}
---
+++ services/types.go
@@ -0,0 +1,12 @@
+package services
+
+type componentEndpoints struct {
+	Total        int      `json:"total"`
+	Available    *podInfo `json:"available"`
+	NotAvailable *podInfo `json:"not_available"`
+}
+
+type podInfo struct {
+	Total int      `json:"total"`
+	Pods  []string `json:"pods"`
+}
